a81cb43489544d66361d19b9e6e4c5a88d2b0df0
btli <bryan.li@gmail.com>
2019-11-21 10:17:52 -0800

    Update sync.py
    Updated to support new pychromecast lib
    Use cast name instead of IP

--- CastPowerSync/sync.py
+++ CastPowerSync/sync.py
@@ -5,58 +5,75 @@ import time
 import pychromecast
 import urllib.request
 
-class CastDevice:
-  def __init__(self, zone, ipAddress):
-    self.status = ''
+class SyncCastDevice:
+  def __init__(self, zone, cc):
     self.zone = zone
-    self.ip = ipAddress
-    self.castDevice = pychromecast.Chromecast(ipAddress) 
-    self.mc = self.castDevice.media_controller.status
+    self.device = cc
     self.baseUrl = 'http://localhost:5000/api/control/'
+    self.status = 'off'
 
-Chromecasts = [
-        CastDevice('1','172.16.11.1'),
-        CastDevice('2','172.16.11.2'),
-        CastDevice('3','172.16.11.3'),
-        CastDevice('4','172.16.11.4')
-]
+print("Initializing...")
+print("Getting all Chromecasts on the network.")
+all_ccs = pychromecast.get_chromecasts()
+
+"""
+  Assign relevant Chromecasts/Groups
+  Zones assigned in order of list/tuple
+"""
+cc_list = ("Dining Room group", "Guest Bathroom group", "Office group", "Master Bathroom group")
+cc_devices = []
+
+for cc in all_ccs:
+  for sel in cc_list:
+    if(cc.device.friendly_name == sel):
+      zone = cc_list.index(cc.device.friendly_name) + 1
+      print("Adding Zone " + str(zone) + ": " + sel)
+      cc_devices.append(SyncCastDevice(zone, cc))
+
+if len(cc_devices) < len(cc_list):
+  print("Could not find all Chromecasts... exiting")
+  sys.exit(1)
+
+cc_devices = sorted(cc_devices, key=lambda cc: cc.zone)
+
+print("Done.\n\n")
 
 while True:
-  print("Checking Chromecasts")
-  i = 0
-  while i < len(Chromecasts):
-    cast = Chromecasts[i]
-    print("IP: " + cast.ip)
-    if not cast.castDevice.status.status_text:
-      currStatus = 'off'
-    else:
+  for cast in cc_devices:
+    cast.device.wait()
+    if cast.device.media_controller.status.player_state == "PLAYING":
       currStatus = 'on'
+    else:
+      currStatus = 'off'
 
     if cast.status != currStatus:
-      reqUrl = cast.baseUrl + cast.zone + '/power/' + currStatus
-      print(reqUrl)
+      print("zone" + str(cast.zone) + ":power_on")
+      reqUrl = cast.baseUrl + str(cast.zone) + '/power/' + currStatus
       with urllib.request.urlopen(reqUrl) as response:
         resp = response.read()
 
       if currStatus == 'on':
-        reqUrl = cast.baseUrl + cast.zone + '/channel/' + cast.zone
-        print(reqUrl)
+        reqUrl = cast.baseUrl + str(cast.zone) + '/channel/' + str(cast.zone)
         with urllib.request.urlopen(reqUrl) as response:
           resp = response.read()
-        reqUrl = cast.baseUrl + cast.zone + '/volume/15'
-        print(reqUrl)
+        reqUrl = cast.baseUrl + str(cast.zone) + '/volume/15'
         with urllib.request.urlopen(reqUrl) as response:
           resp = response.read()
 
       cast.status = currStatus 
 
+    message = "zone" + str(cast.zone) + ":" + cast.device.device.friendly_name + ": "
     if currStatus == 'on':
       try:
-        print("Now Playing: " + cast.mc.artist + " - " + cast.mc.title)
+        message += "playing:"
+        message += cast.device.media_controller.status.artist.replace(" ", "_").lower()
+        message += "/" + cast.device.media_controller.status.title.replace(" ", "_").lower()
       except:
-        print("Now Playing: Unknown - Unknown")
+        message += "playing:unknown/unknown"
     else:
-      print("Nothing playing.")
-    i += 1
-  time.sleep(5)
+      message += "idle"
 
+    print(message)
+  print("")
+  time.sleep(10)
+  
